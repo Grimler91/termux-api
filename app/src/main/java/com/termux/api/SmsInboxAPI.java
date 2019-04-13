007efac596d34d924ee9ec2843ca577b7c514a3f
Henrik Grimler <henrik@grimler.se>
2019-04-13 09:39:32 +0200

    Revert "Remove call log and SMS permissions"
    This reverts commit de44896a01111506590a258f0267400af067b778.

--- app/src/main/java/com/termux/api/SmsInboxAPI.java
+++ app/src/main/java/com/termux/api/SmsInboxAPI.java
@@ -1,23 +1,136 @@
 package com.termux.api;
 
+import android.annotation.SuppressLint;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.ContactsContract.PhoneLookup;
+import android.provider.Telephony;
+import android.provider.Telephony.TextBasedSmsColumns;
 import android.util.JsonWriter;
 
 import com.termux.api.util.ResultReturner;
 import com.termux.api.util.ResultReturner.ResultJsonWriter;
 
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
 public class SmsInboxAPI {
 
+    private static final String[] DISPLAY_NAME_PROJECTION = {PhoneLookup.DISPLAY_NAME};
+
     static void onReceive(final Context context, JSONObject opts) {
+        final int offset = opts.optInt("offset", 0);
+        final int limit = opts.optInt("limit", 50);
+        final Uri contentURI = typeToContentURI(opts.optInt("type", TextBasedSmsColumns.MESSAGE_TYPE_INBOX));
+
         ResultReturner.returnData(context, new ResultJsonWriter() {
             @Override
             public void writeJson(JsonWriter out) throws Exception {
-                out.beginObject();
-                out.name("error").value("Reading SMS is no longer permitted by Google");
-                out.endObject();
+                getAllSms(context, out, offset, limit, contentURI);
             }
         });
     }
+
+    @SuppressLint("SimpleDateFormat")
+    public static void getAllSms(Context context, JsonWriter out, int offset, int limit, Uri contentURI) throws IOException {
+        ContentResolver cr = context.getContentResolver();
+        String sortOrder = "date DESC LIMIT + " + limit + " OFFSET " + offset;
+        try (Cursor c = cr.query(contentURI, null, null, null, sortOrder)) {
+
+            c.moveToLast();
+
+            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd kk:mm");
+            Map<String, String> nameCache = new HashMap<>();
+
+            out.beginArray();
+            for (int i = 0, count = c.getCount(); i < count; i++) {
+                int threadID = c.getInt(c.getColumnIndexOrThrow(TextBasedSmsColumns.THREAD_ID));
+                String smsAddress = c.getString(c.getColumnIndexOrThrow(TextBasedSmsColumns.ADDRESS));
+                String smsBody = c.getString(c.getColumnIndexOrThrow(TextBasedSmsColumns.BODY));
+                boolean read = (c.getInt(c.getColumnIndex(TextBasedSmsColumns.READ)) != 0);
+                long smsReceivedDate = c.getLong(c.getColumnIndexOrThrow(TextBasedSmsColumns.DATE));
+                // long smsSentDate = c.getLong(c.getColumnIndexOrThrow(TextBasedSmsColumns.DATE_SENT));
+
+                String smsSenderName = getContactNameFromNumber(nameCache, context, smsAddress);
+                String messageType = getMessageType(c.getInt(c.getColumnIndexOrThrow(TextBasedSmsColumns.TYPE)));
+
+                out.beginObject();
+                out.name("threadid").value(threadID);
+                out.name("type").value(messageType);
+                out.name("read").value(read);
+
+                if (smsSenderName != null) {
+                    out.name("sender").value(smsSenderName);
+                }
+                out.name("number").value(smsAddress);
+
+                out.name("received").value(dateFormat.format(new Date(smsReceivedDate)));
+                // if (Math.abs(smsReceivedDate - smsSentDate) >= 60000) {
+                // out.write(" (sent ");
+                // out.write(dateFormat.format(new Date(smsSentDate)));
+                // out.write(")");
+                // }
+                out.name("body").value(smsBody);
+
+                c.moveToPrevious();
+                out.endObject();
+            }
+            out.endArray();
+        }
+    }
+
+    private static String getContactNameFromNumber(Map<String, String> cache, Context context, String number) {
+        if (cache.containsKey(number))
+            return cache.get(number);
+        Uri contactUri = Uri.withAppendedPath(PhoneLookup.CONTENT_FILTER_URI, Uri.encode(number));
+        try (Cursor c = context.getContentResolver().query(contactUri, DISPLAY_NAME_PROJECTION, null, null, null)) {
+            String name = c.moveToFirst() ? c.getString(c.getColumnIndex(PhoneLookup.DISPLAY_NAME)) : null;
+            cache.put(number, name);
+            return name;
+        }
+    }
+
+    private static String getMessageType(int type) {
+        switch (type)
+        {
+            case TextBasedSmsColumns.MESSAGE_TYPE_INBOX:
+                return "inbox";
+            case TextBasedSmsColumns.MESSAGE_TYPE_SENT:
+                return "sent";
+            case TextBasedSmsColumns.MESSAGE_TYPE_DRAFT:
+                return "draft";
+            case TextBasedSmsColumns.MESSAGE_TYPE_FAILED:
+                return "failed";
+            case TextBasedSmsColumns.MESSAGE_TYPE_OUTBOX:
+                return "outbox";
+            default:
+                return "";
+        }
+    }
+
+    private static Uri typeToContentURI(int type) {
+        switch (type) {
+            case TextBasedSmsColumns.MESSAGE_TYPE_ALL:
+                return Telephony.Sms.CONTENT_URI;
+            case TextBasedSmsColumns.MESSAGE_TYPE_SENT:
+                return Telephony.Sms.Sent.CONTENT_URI;
+            case TextBasedSmsColumns.MESSAGE_TYPE_DRAFT:
+                return Telephony.Sms.Draft.CONTENT_URI;
+            case TextBasedSmsColumns.MESSAGE_TYPE_OUTBOX:
+                return Telephony.Sms.Outbox.CONTENT_URI;
+            case TextBasedSmsColumns.MESSAGE_TYPE_INBOX:
+            default:
+                return Telephony.Sms.Inbox.CONTENT_URI;
+        }
+    }
+
 
 }
--- app/src/main/java/com/termux/api/SmsSendAPI.java
+++ app/src/main/java/com/termux/api/SmsSendAPI.java
@@ -2,19 +2,44 @@ package com.termux.api;
 
 import android.content.Context;
 import android.content.Intent;
-import android.util.JsonWriter;
+import android.telephony.SmsManager;
 
 import com.termux.api.util.ResultReturner;
+import com.termux.api.util.TermuxApiLogger;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.PrintWriter;
+import java.util.ArrayList;
 
 public class SmsSendAPI {
 
-    static void onReceive(Context context final JSONObject opts) {
-        ResultReturner.returnData(apiReceiver, intent, new ResultReturner.ResultJsonWriter() {
+    static void onReceive(Context context, final JSONObject opts) {
+        ResultReturner.returnData(context, new ResultReturner.WithStringInput() {
             @Override
-            public void writeJson(JsonWriter out) throws Exception {
-                out.beginObject();
-                out.name("error").value("Sending SMS is no longer permitted by Google");
-                out.endObject();
+            public void writeResult(PrintWriter out) {
+                final SmsManager smsManager = SmsManager.getDefault();
+                JSONArray recipientsJson = opts.optJSONArray("recipients");
+                String[] recipients = new String[recipientsJson.length()];
+                for(int i = 0; i < recipientsJson.length(); i++){
+                    recipients[i] = recipientsJson.optString(i);
+                }
+
+                if (recipients == null) {
+                    // Used by old versions of termux-send-sms.
+                    String recipient = opts.optString("recipient");
+                    if (recipient != null) recipients = new String[]{recipient};
+                }
+
+                if (recipients == null || recipients.length == 0) {
+                    TermuxApiLogger.error("No recipient given");
+                } else {
+                    final ArrayList<String> messages = smsManager.divideMessage(inputString);
+                    for (String recipient : recipients) {
+                        smsManager.sendMultipartTextMessage(recipient, null, messages, null, null);
+                    }
+		}
             }
         });
     }
